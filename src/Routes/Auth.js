c2e0291dea17e6239028e2eb018a7deb440e8440
tonky0110 <tonky0110@gmail.com>
2019-10-04 16:53:44 +0900

    5.0 Auth Route and css

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,1 +1,17 @@
-export default () => 'Auth';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	min-height: 80vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Box = styled.div`${(props) => props.theme.whiteBox};`;
+
+export default () => {
+	const [ action, setAction ] = useState('logIn');
+	// action === 'logIn' ? 'Log In' : 'Sign Up'
+	return <Wrapper>{action === 'logIn' ? <Box>Log In</Box> : <Box>Sign Up</Box>}</Wrapper>;
+};
